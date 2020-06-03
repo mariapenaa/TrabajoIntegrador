b481e4a163fd7a180f81432bbe26ee3ccd801f91
Maria Peña <mpena@udesa.edu.ar>
2020-06-02 22:47:30 -0300

    linkear tracks con detail.html

--- js/detail-song.js
+++ js/detail-song.js
@@ -1,7 +1,29 @@
 window.onload = function(){
-
 var queryString = location.search
-console.log(queryString)
+//var queryStringObj = new URLSearchParams(queryString)
+
+fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/")
+.then(function(response) {
+    return response.json() 
+  })
+
+    .then(function(resultado) {
+
+      for (let i = 0; i < resultado.data.length; i++) {
+        var trackTitle  = resultado.data[i].title ;
+        console.log(queryString);
+        var nombreCancion = document.querySelector(".nombre");
+        
+        nombreCancion.innerHTML+=trackTitle;
+
+      }
+   })
+
+   .catch(function(error) {
+     console.log("Error: " + error);
+   })
+
+
 
 
 
