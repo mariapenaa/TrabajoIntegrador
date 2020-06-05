eda1be4cc049c57d4fe8b90489aa58b1dd71f53f
Felicitas Rattagan <frattagan@udesa.edu.ar>
2020-06-05 16:44:02 -0300

    playlist

--- js/playlist.js
+++ js/playlist.js
@@ -1,3 +1,13 @@
 window.onload= function(){
+var userTitle = document.querySelector(".playlistTitle")
+var username= localStorage.getItem("username")
+if (username != "" && username !=null) {
+    userTitle.innerHTML+= username+"'s Playlist"
+    userTitle.style.textTransform = 'capitalize'
+} else {
+    userTitle.innerHTML+= "My Playlist"
+}
+
+var 
 
 }
