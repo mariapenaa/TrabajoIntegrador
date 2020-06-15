4be9a73cd20728d253a7dcdd21bf768258271999
Maria Peña <mpena@udesa.edu.ar>
2020-06-14 22:54:44 -0300

    genre

--- js/genres.js
+++ js/genres.js
@@ -6,12 +6,14 @@ window.onload = function(){
       })
     
         .then(function(resultado) {
-            for(let i = 0; i<resultado.data.length; i++){
-                console.log(resultado)
+            for(let i = 0; i< resultado.data.length; i++){
                 var genreCard = document.querySelector(".genre");
                 var genreName = resultado.data[i].name;
                 var genreFoto = resultado.data[i].picture_big;
                 var genreId = resultado.data[i].id;
+                console.log(genreName)
+                 //cierra primer for
+                  
             }
                 fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/"+genreId+"/artists")
                  .then(function(response) {
@@ -19,16 +21,16 @@ window.onload = function(){
                   })
                 
                     .then(function(resultado) {
+                        
                        for(let i = 0; i<resultado.data.length; i++){
-                            console.log(resultado)
                             var topArtist = resultado.data[i].picture_big
                             genreCard.innerHTML+="<div class='genre-card-single'><div class='uk-text-center uk-card uk-card-default uk-card-body genre-div uk-cover-container'>"
                             +" <div class='uk-inline-clip uk-transition-toggle' tabindex='0'><img src='"+genreFoto+"' alt='genre foto' uk-cover>"
                             +"<img class='uk-transition-scale-up uk-position-cover' src='"+topArtist+"' alt='otra foto' uk-cover></div>"
                             +"<p class='uk-margin-small-top'><a href='detailsong.html?id="+genreId+"&type=genre'>"+genreName+"</a></p></div>";
-                       }//cierra for
-              
-        
+                      }//cierra segundo for
+                    
+                    
                     
                    })//cierra then
                 
@@ -36,9 +38,8 @@ window.onload = function(){
                      console.log("Error: " + error);
                    })//cierra then
             
-            
-   
-          //cierra for
+                  
+               
        })//cierra then
     
        .catch(function(error) {
