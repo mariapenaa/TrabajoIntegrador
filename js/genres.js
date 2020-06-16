d629ad70323b36363d28da09ef39501a4fc953a4
Maria Peña <mpena@udesa.edu.ar>
2020-06-16 12:56:43 -0300

    genre

--- js/genres.js
+++ js/genres.js
@@ -7,15 +7,11 @@ window.onload = function(){
       })
     
         .then(function(resultado) {
-            for(let i = 0; i< resultado.data.length; i++){
-                var genreCard = document.querySelector(".genre");
-                var genreName = resultado.data[i].name;
-                var genreFoto = resultado.data[i].picture_big;
-                var genreId = resultado.data[i].id;
-                console.log(genreName)
-             
+            
+                var genreArray = []
                 
-            } //cierra primer for
+  
+             //cierra primer for
 
                 fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/"+genreId+"/artists")
                  .then(function(response) {
@@ -23,14 +19,11 @@ window.onload = function(){
                   })
                 
                     .then(function(resultado) {
-                        
+                      
+                     
                        for(let i = 0; i<resultado.data.length; i++){
-                         console.log(genreName)
                             var topArtist = resultado.data[i].picture_big
-                            genreCard.innerHTML+="<div class='genre-card-single'><div class='uk-text-center uk-card uk-card-default uk-card-body genre-div uk-cover-container'>"
-                            +" <div class='uk-inline-clip uk-transition-toggle' tabindex='0'><img src='"+genreFoto+"' alt='genre foto' uk-cover>"
-                            +"<img class='uk-transition-scale-up uk-position-cover' src='"+topArtist+"' alt='otra foto' uk-cover></div>"
-                            +"<p class='uk-margin-small-top'><a href='detailsong.html?id="+genreId+"&type=genre'>"+genreName+"</a></p></div>";
+                            genreArray.push(topArtist)
                       }//cierra segundo for
                     
                     
@@ -40,7 +33,23 @@ window.onload = function(){
                    .catch(function(error) {
                      console.log("Error: " + error);
                    })//cierra then
-            
+                   
+                   for(let i = 0; i< resultado.data.length; i++){
+                    var genreCard = document.querySelector(".genre");
+                    var genreName = resultado.data[i].name;
+                    var genreFoto = resultado.data[i].picture_big;
+                    var genreId = resultado.data[i].id;
+                    console.log(genreArray[0])
+                    genreCard.innerHTML+="<div class='genre-card-single'><div class='uk-text-center uk-card uk-card-default uk-card-body genre-div uk-cover-container'>"
+                    +" <div class='uk-inline-clip uk-transition-toggle' tabindex='0'><img src='"+genreFoto+"' alt='genre foto' uk-cover>"
+                    +"<img class='uk-transition-scale-up uk-position-cover' src='"+genreArray[i]+"' alt='otra foto' uk-cover></div>"
+                    +"<p class='uk-margin-small-top'><a href='detailsong.html?id="+genreId+"&type=genre'>"+genreName+"</a></p></div>"; 
+                  
+                   }
+
+                  
+
+
                   
                
        })//cierra then
